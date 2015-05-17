52ed1bec43cdb79d93452085afb878569ef1af47
maxingenio <mangetor@gmail.com>
2015-05-17 20:10:50 +0200

    Holaaa holaa

--- app/src/main/java/ask4some/ask4some/MainActivity2.java
+++ app/src/main/java/ask4some/ask4some/MainActivity2.java
@@ -1,30 +1,22 @@
 package ask4some.ask4some;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity2 extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
 
-    public void sendMessage(View view)
-    {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-        startActivity(intent);
-    }
 
-    @Override
+    @Override2
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
